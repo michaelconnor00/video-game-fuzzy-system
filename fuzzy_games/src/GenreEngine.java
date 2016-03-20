4cad02fedbd75760f12e111c8dea49ee02fa57b4
Michael Connor <mike@sparkgeo.com>
2016-03-20 12:49:58 -0700

    Added shell for the Genre Inference Engine

---
+++ fuzzy_games/src/GenreEngine.java
@@ -0,0 +1,16 @@
+/**
+ * Class that contains the Inference logic
+ * Created by michaelconnor on 2016-03-20.
+ */
+public class GenreEngine {
+
+    public GenreEngine(){ }
+
+    public Genre getGenre(int[] userTraitValues){
+        for (Genre gen : Genre.values()){
+            // TODO get top rated genres?????
+        }
+        return Genre.ROLEPLAYING;
+    }
+
+}
