f8a078f1bd5b76cbab5a4d02f5a45d11bf6f79dc
Michael Connor <mike@sparkgeo.com>
2016-03-20 12:49:18 -0700

    Added enum for behaviour traits

---
+++ fuzzy_games/src/BehaviouralTraits.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+/**
+ * Enum to represent the persons behavioural traits that relate to Gaming.
+ * Created by michaelconnor on 2016-03-20.
+ */
+public enum BehaviouralTraits {
+    ATTENTIONTODETAIL,
+    PATIENCE,
+    REACTIONTIME,
+    ANXIETY,
+    PERSISTANCE,
+    EXCITEMENT,
+    COMPETITIVENESS,
+    PLANNING,
+    TEAMWORK
+}
