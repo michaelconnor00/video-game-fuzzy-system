1c7a17700ae04939e5f374840027f4cf2520f873
Michael Connor <mike@sparkgeo.com>
2016-03-20 12:50:56 -0700

    enum for trait values

---
+++ fuzzy_games/src/TraitValue.java
@@ -0,0 +1,22 @@
+/**
+ * Enum for standard trait values
+ * Created by michaelconnor on 2016-03-20.
+ */
+public enum TraitValue {
+    INEPT("Inept..."), // Lowest
+    LOW("Embarrassing"),
+    AVERAGE("I can get by"),
+    HIGH("Bring it on"),
+    GODLIKE("God Like!"); // Highest
+
+    private String displayName;
+
+    TraitValue(String display){
+        this.displayName = display;
+    }
+
+    public String toString(){
+        return this.displayName;
+    }
+
+}
