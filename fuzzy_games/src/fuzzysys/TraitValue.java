e7b0f48c591203500c71f732ea65a39ce5c43197
Michael Connor <mike@sparkgeo.com>
2016-04-02 16:12:53 -0700

    Add membership function to TraitValue enum.

--- fuzzy_games/src/fuzzysys/TraitValue.java
+++ fuzzy_games/src/fuzzysys/TraitValue.java
@@ -6,10 +6,9 @@ package fuzzysys;
  */
 public enum TraitValue {
 
-    FAR("big difernce"),//great differince between user value and arch type
-    NEAR("small diferince"),//small differince between user value and arch type
-    PERFECT("perfict match");//no differince between user value and arch type
-
+    HIGH("God Like"),
+    MEDIUM("Could Care Less"),
+    LOW("Inept");
 
     private String displayName;
 
@@ -17,11 +16,43 @@ public enum TraitValue {
         this.displayName = display;
     }
 
+    private double getMembership(double x){
+        switch (this.name()){
+            case "HIGH":
+                return highMembershipFunc(x);
+            case "LOW":
+                return lowMembershipFunc(x);
+            case "MEDIUM":
+                return mediumMembershipFunc(x);
+        }
+        return -1.0;
+    }
+
+    private double highMembershipFunc(double x){
+        if (x <= 2.0){
+            return 0.0;
+        }
+
+        return (x - 2.0) / 2.0;
+    }
+
+    private double lowMembershipFunc(double x){
+        if (x >= 2.0){
+            return 0.0;
+        }
+
+        return (2.0 - x) / 2.0;
+    }
+
+    private double mediumMembershipFunc(double x){
+        if (x <= 2.0){
+            return x / 2.0;
+        }
+
+        return (4.0 - x) / 2.0;
+    }
+
     public String toString(){
         return this.displayName;
     }
-
-
-
-
 }
